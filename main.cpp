2cf845b0528790c7d8856dd67b57e3fb54fd3291
baku1413 <33036538+baku1413@users.noreply.github.com>
2017-11-13 20:30:23 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -25,39 +25,76 @@
 
 #include <iostream>
 #include <ncurses/ncurses.h>
+#include <string>
 using namespace std;
 int c;
 int x=10; //start pos
-int y=10;
-const int szerokosc = 80;
-const int wysokosc = 40;
-bool koniec = false;
+int y=0;
+int moc = 0;
+string naped;
+bool gra = true;
 
 int main()
 {
-    initscr();
+
+
+            if (moc > 3)
+            {
+                moc = 0;
+
+            }
+            initscr();
+            clear();
+
+
+            move(y,x);
+            printw("A");
+            move(y+1, x);
+            printw("%d", naped);
+
+            if (moc = 0)
+            {
+                string naped="o";
+
+            }
+            if (moc = 1)
+            {
+                string naped="o";
+            }
+            if (moc = 2)
+            {
+                string naped="O";
+            }
+            if (moc = 3)
+            {
+                string naped="0";
+            }
+
+
+            c=getch();
+
+
 
     while(c != 27) //ESC = exit
     {
-            clear();
-            move(y,x);
-            refresh();
-            printw("A");
 
-            c=getch();
+
             switch(c)
             {
                 case 119://w
                     y--;
                     main();
+
                 break;
                 case 97://a
                     x--;
                     main();
+
                 break;
                 case 115://s
-                    y++;
+                    moc++;
                     main();
+
                 break;
                 case 100://d
                     x++;
@@ -67,7 +104,7 @@ int main()
             }
     }
     getch();
-    opad();
+    main();
     endwin();
     return 0;
 
@@ -77,10 +114,3 @@ int main()
 }
 
 
-void opad()
-{
-   while(koniec != false)
-   {
-   y++;
-   }
-}
